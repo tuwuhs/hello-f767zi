1b798b3a20b4476b4856f637948c22c23d9f575a
Tuwuh Sarwoprasojo <tuwuhs@gmail.com>
2020-06-24 16:13:09 -0400

    Configure clock to run at 216 MHz (using PLL and HSE)

--- src/main.rs
+++ src/main.rs
@@ -25,35 +25,80 @@ fn main() -> ! {
 
     let cp = cortex_m::Peripherals::take().unwrap();
     let p = stm32f7x7::Peripherals::take().unwrap();
+    
+    let rcc = p.RCC;
+    let pwr = p.PWR;
+    
+    // Enable PWR, set voltage scale
+    rcc.apb1enr.modify(|_, w| w.pwren().set_bit());
+    pwr.cr1.modify(|_, w| w.vos().scale1());
 
+    // Enable HSE
+    rcc.cr.modify(|_, w| w.hsebyp().set_bit().hseon().set_bit());
+    while rcc.cr.read().hserdy().is_not_ready() {};
+
+    // Configure PLL
+    rcc.cr.modify(|_, w| w.pllon().clear_bit());
+    rcc.pllcfgr.write(|w| unsafe {
+        w.pllsrc().hse().
+        pllm().bits(4).
+        plln().bits(216).
+        pllp().div2().
+        pllq().bits(9)
+    });
+    rcc.cr.modify(|_, w| w.pllon().set_bit());
+    while rcc.cr.read().pllrdy().is_not_ready() {};
+
+    // Enable overdrive
+    pwr.cr1.modify(|_, w| w.oden().set_bit());
+    while pwr.csr1.read().odrdy().bit_is_clear() {};
+    pwr.cr1.modify(|_, w| w.odswen().set_bit());
+    while pwr.csr1.read().odswrdy().bit_is_clear() {};
+
+    // Configure clock and flash latency
+    let flash = p.FLASH;
+    let flash_latency : u8 = 7;
+    if flash_latency > flash.acr.read().latency().bits() {
+        flash.acr.modify(|_, w| { w.latency().bits(flash_latency) });
+    }
+    rcc.cfgr.modify(|_, w| w.
+        ppre1().div16().
+        ppre2().div16().
+        hpre().div1().
+        sw().pll()
+    );
+    while !rcc.cfgr.read().sw().is_pll() {};
+    if flash_latency < flash.acr.read().latency().bits() {
+        flash.acr.modify(|_, w| { w.latency().bits(flash_latency) });
+    }
+    rcc.cfgr.modify(|_, w| w.
+        ppre1().div4().
+        ppre2().div2()
+    );
+
+    // SysTick initialization
     let mut systick = cp.SYST;
     systick.set_clock_source(syst::SystClkSource::Core);
-    systick.set_reload(8_000_000);
+    systick.set_reload(8_000_000 - 1);
     systick.clear_current();
     systick.enable_counter();
 
-    let rcc = p.RCC;
-    rcc.ahb1enr.modify(|_, w| w.
-        gpioben().set_bit().
-        gpiocen().set_bit()
-    );
+    // GPIO initialization
+    rcc.ahb1enr.modify(|_, w| w.gpioben().set_bit().gpiocen().set_bit());
 
     let gpiob = p.GPIOB;
     gpiob.ospeedr.modify(|_, w| w.
         ospeedr0().low_speed().
         ospeedr7().low_speed().
-        ospeedr14().low_speed()
-    );
+        ospeedr14().low_speed());
     gpiob.pupdr.modify(|_, w| w.
         pupdr0().floating().
         pupdr7().floating().
-        pupdr14().floating()
-    );
+        pupdr14().floating());
     gpiob.moder.modify(|_, w| w.
         moder0().output().
         moder7().output().
-        moder14().output()
-    );
+        moder14().output());
 
     let gpioc = p.GPIOC;
     gpioc.pupdr.modify(|_, w| w.pupdr13().floating());
@@ -86,9 +131,11 @@ fn main() -> ! {
             }
         }
 
-        // while !systick.has_wrapped() {};
+        // // while !systick.has_wrapped() {};
+        // cortex_m::asm::delay(24_000_000);
         // gpiob.bsrr.write(|w| w.bs0().set_bit());
-        // while !systick.has_wrapped() {};
+        // // while !systick.has_wrapped() {};
+        // cortex_m::asm::delay(24_000_000);
         // gpiob.bsrr.write(|w| w.br0().set_bit());        
     }
 }
