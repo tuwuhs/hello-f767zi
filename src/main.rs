33e5992a3bbc27986d2390a3c1d359cefe5ed41b
Tuwuh Sarwoprasojo <tuwuhs@gmail.com>
2020-06-26 15:00:42 -0400

    Rename LEDs

--- src/main.rs
+++ src/main.rs
@@ -61,51 +61,36 @@ fn main() -> ! {
     let gpiob = p.GPIOB.split();
     let gpioc = p.GPIOC.split();
     
-    let mut led1 = gpiob.pb0.into_push_pull_output();
-    let mut led2 = gpiob.pb7.into_push_pull_output();
-    let mut led3 = gpiob.pb14.into_push_pull_output();
+    let mut led_green = gpiob.pb0.into_push_pull_output();
+    let mut led_blue = gpiob.pb7.into_push_pull_output();
+    let mut led_red = gpiob.pb14.into_push_pull_output();
     let button = gpioc.pc13.into_floating_input();
-
-    led2.set_high().unwrap();
+    
+    led_blue.set_high().unwrap();
 
     let mut heartbeat_timer = get_millis();
 
     loop {
-        // if button.is_high().unwrap() {
-        //     led3.set_high().unwrap();
-        // } else {
-        //     led3.set_low().unwrap();
-        // }
-
         match button.is_high() {
-            Ok(false) => led3.set_low().unwrap(),
-            Ok(true) => led3.set_high().unwrap(),
+            Ok(false) => led_red.set_low().unwrap(),
+            Ok(true) => led_red.set_high().unwrap(),
             _ => ()
         }
 
         if has_elapsed(heartbeat_timer, 500) {
             heartbeat_timer = get_millis();
 
-            match led1.is_high() {
-                Ok(false) => led1.set_high().unwrap(),
-                Ok(true) => led1.set_low().unwrap(),
+            match led_green.is_high() {
+                Ok(false) => led_green.set_high().unwrap(),
+                Ok(true) => led_green.set_low().unwrap(),
                 _ => ()
             }
 
-            if led2.is_high().unwrap() {
-                led2.set_low().unwrap();
+            if led_blue.is_high().unwrap() {
+                led_blue.set_low().unwrap();
             } else {
-                led2.set_high().unwrap();
+                led_blue.set_high().unwrap();
             }
-        }
-
-        // while !systick.has_wrapped() {};
-        // cortex_m::asm::delay(24_000_000);
-        // delay_ms(1000);
-        // gpiob.bsrr.write(|w| w.bs0().set_bit());
-        // while !systick.has_wrapped() {};
-        // cortex_m::asm::delay(24_000_000);
-        // delay_ms(1000);
-        // gpiob.bsrr.write(|w| w.br0().set_bit());        
+        }   
     }
 }
